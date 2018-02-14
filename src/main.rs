1aed8569c34b13947219972a408b7c3476791be5
Peter Lyons <pete@peterlyons.com>
2018-02-14 10:54:36 -0700

    map and map err

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+
+#[derive(Debug)]
+enum NomError {
+    Failed1(String),
+    Failed2(String)
+}
+
+// models nom_pem::decode_block
+fn decode_block(input: &str) -> Result<String, NomError> {
+    Ok(input.to_uppercase())
+}
+
+#[derive(Debug)]
+enum ParseError {
+    Bummer(String)
+}
+
+// models x509_parser::x509_parser
+fn x509_parser(data: String) -> Result<char, ParseError> {
+    Ok(data.chars().nth(0).unwrap_or('Z'))
+}
+
 fn main() {
     println!("construct");
+    println!(
+        "{:?}",
+        decode_block("hey")
+            .map(|upper| x509_parser(upper))
+            .map(|a_char| 'X')
+            .map_err(|nom_error| format!("{:?}", nom_error))
+            // .and_then(|a_char| a_char)
+    );
 }
